95ed7251e2fb7a423b8e0fee991dadf503379e92
Pranav <pranavkale1124@gmail.com>
2023-11-16 21:02:37 +0530

    Added footer

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import Header from "../components/indexpagecomponents/Header";
 import StudentIndexPage from "./StudentIndexPage";
 import ProctorIndexPage from "./ProctorIndexPage";
+import Footer from "../components/indexpagecomponents/Footer";
 
 const IndexPage = () => {
   const [role, setRole] = useState("");
 
   return (
-    <div className="max-w-6xl mx-auto p-4 border border-black">
+    <div className=" max-w-6xl mx-auto p-4 border border-black">
       {role === "student" ? <StudentIndexPage /> : null}
       {role === "proctor" ? <ProctorIndexPage /> : null}
       <Header />
+      <Footer/>
     </div>
   );
 };
